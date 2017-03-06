b2350adc13bdfd9b1909b698dc068927ea51bc4a
Alex Ruzzarin <alex@ruzzarin.net>
2017-03-06 02:40:48 -0300

    homebridge improvements

--- build/homebridge/image/run.sh
+++ build/homebridge/image/run.sh
@@ -8,4 +8,14 @@ avahi-daemon -D
 service dbus start
 service avahi-daemon start
 
+install_file="/root/.homebridge/install.sh"
+if [ -f "$install_file" ]
+then
+    echo "Executing $install_file."
+
+    sh $install_file
+else
+    echo "$install_file not found."
+fi
+
 homebridge
