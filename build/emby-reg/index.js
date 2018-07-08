4792ab0e7628c04b6d775d2bb70b7ac476395dee
Alex <1187547+alexruzzarin@users.noreply.github.com>
2018-07-07 21:40:40 -0300

    emby-reg add https

--- build/emby-reg/index.js
+++ build/emby-reg/index.js
@@ -4,16 +4,21 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 
-// const privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-// const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+const privateKey = fs.readFileSync('cert/emby-reg.key', 'utf8');
+const certificate = fs.readFileSync('cert/emby-reg.cert', 'utf8');
 
-// const credentials = {key: privateKey, cert: certificate};
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  requestCert: false,
+  rejectUnauthorized: false
+};
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.all("/*", function(req, res) {
+app.all("/*", function (req, res) {
   res.send({
     featId: req.body.feature,
     registered: true,
@@ -23,7 +28,7 @@ app.all("/*", function(req, res) {
 });
 
 const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(credentials, app);
+const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(8080);
-// httpsServer.listen(8443);
+httpsServer.listen(8443);
